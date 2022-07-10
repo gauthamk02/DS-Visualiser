2c7f0d5f77430d56aa23682f677fae76730e9608
Gautham Krishnan <gauthamkrishnanpriya@gmail.com>
2022-07-11 02:00:10 +0530

    Updates

---
+++ precache-manifest.1af57def2d9319790d29418d5c7d6318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "60186188878976d9596b",
+    "url": "/DS-Visualiser/static/css/main.bf1f9536.chunk.css"
+  },
+  {
+    "revision": "60186188878976d9596b",
+    "url": "/DS-Visualiser/static/js/main.f5f3a1f8.chunk.js"
+  },
+  {
+    "revision": "cdca18806bdaef7eaab4",
+    "url": "/DS-Visualiser/static/js/runtime~main.5a30fd0d.js"
+  },
+  {
+    "revision": "35b47e952181861caa14",
+    "url": "/DS-Visualiser/static/css/2.ba529c09.chunk.css"
+  },
+  {
+    "revision": "35b47e952181861caa14",
+    "url": "/DS-Visualiser/static/js/2.3361b492.chunk.js"
+  },
+  {
+    "revision": "f610709ef7b15d7cd37f",
+    "url": "/DS-Visualiser/static/js/3.c3bc911a.chunk.js"
+  },
+  {
+    "revision": "e1728266df8621582ba24ce4bb7504d0",
+    "url": "/DS-Visualiser/index.html"
+  }
+];
